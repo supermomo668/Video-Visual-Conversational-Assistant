96d72d383fb9dcef71f200325e27f790973c2696
supermomo668 <mymomo119966.mm@gmail.com>
2023-10-03 05:14:25 +0000

    rm cached

--- llava/llava/putils/print.py
+++
@@ -1,5 +0,0 @@
-
-def print_pload(pload):
-  return {
-    f'Key:{k}\n\t: Value{v}\n\tType:{type(v)}' for k,v in pload.items()
-  }
--- llava/llava/setup.sh
+++
@@ -1,9 +0,0 @@
-env_name=$"{$1:-dl}"
-conda activate $env_name
-pip install --upgrade pip  # enable PEP 660 support
-pip install -e .
-pip install ninja
-pip install flash-attn --no-build-isolation
-# extra dependencies
-sudo apt update && sudo apt install ffmpeg
-pip install -U openai-whisper
